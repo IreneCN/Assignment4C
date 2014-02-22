5dda5f5cbeff0ec299a11edb3f1731a7a2e32c8d
Irene <icn2104@columbia.edu>
2014-02-21 19:39:59 -0500

    I've created a jQuery div in my scripts.js file

--- scripts.js
+++ scripts.js
@@ -1,9 +1,24 @@
 /**
  * @author Chidinma Irene Nwoye
  */
+function pageUp(){
+	
+	//This is my jQuery div
+	var myNewDiv = $("<div>");                         
+	$(myNewDiv).html("I've created a new div");
+	
+	$("#DocumentBox").append(myNewDiv);
+	
+	console.log("my document loads!");
+  	
+  }
 
+
+  $(document).ready(pageUp);
+
+console.log("my javascript file works!");
   
-  console.log("javascript file is functioning!");
+  
 
 
 
