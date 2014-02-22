a3716641153a13038757e39b0edb6fe9175b05c8
Irene <icn2104@columbia.edu>
2014-02-21 21:49:07 -0500

    I've corrected some typos in my javascript file

--- scripts.js
+++ scripts.js
@@ -22,14 +22,14 @@ function pageUp(){
 	//I've also added html i.e. the string          
 	$(myNewDiv).html("I've created a new div");
 	
-	//I've prompted jQuery to put my new div into inside the div with the id, "DocumentBox"
+	//I've prompted jQuery to put my new div inside the div with the id, "DocumentBox"
 	//I expect that the new div will therefore contain the text,"I've created a new div"
 	$("#DocumentBox").append(myNewDiv);
 	
 	
-	//I'm using the jQuery get function to load my dataMovies json file which I created from my Assignment 3 array of objects
-	//It including 3 parameters: the 1st parameter is the name of my json file, dataMovies.json
-	//The 2nd parameter is the function that will be called immediately the file loads i.e. includeJSON which I declared at the top of my code
+	//I'm using the jQuery get function to load my dataMovies json file, which I created from my Assignment 3 array of objects
+	//It includes 3 parameters: the 1st parameter is the name of my json file, dataMovies.json
+	//The 2nd parameter is the function that will be called immediately the file loads i.e. includeJSON, which I declared at the top of my code
 	//The 3rd parameter is a string of the file type that is expected
 	$.get("dataMovies.json",includeJSON,"json"); 
   	
